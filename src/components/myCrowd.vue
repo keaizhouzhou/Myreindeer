b34143fcd16d9322a674f03ca2cd1090005f1b81
zhouwei <zhouwei@huli.com>
2018-07-30 23:07:19 +0800

    我的订单,我的众筹

--- src/components/myCrowd.vue
+++ src/components/myCrowd.vue
@@ -4,78 +4,71 @@
     <houseSort :tabList="tabList" @tabClick="tabClick ($event)"></houseSort>
     <div class="content">
       <div v-if="selectedTab.key == 'crowding'">
-        <div class="itemWrap">
+        <div class="itemWrap" v-for="(item,index) in crowdingItems">
           <div class="home-item">
-            <div class="item-image"></div>
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.SmallImgUrl+')' ,backgroudSize:'cover'}"></div>
             <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0</div>
+              <div class="tittle">{{item.MName}}</div>
+              <div class="price">￥{{item.Price}}</div>
               <div class="progress-parent">
-                <div class="icon">50%</div>
-                <div class="progress-child"></div>
+                <div class="icon">{{item.Rate}}</div>
+                <div class="progress-child" :style="{width:item.Rate}"></div>
               </div>
-              <div class="need_price">还差10000.00元</div>
+              <div class="need_price">还差{{item.Unfinished}}元</div>
             </div>
           </div>
           <div class="btns">
-            <div class="myself_support" @click="jumpSelfSupport">自己支持</div>
-            <div class="go_crowd" @click="jumpSharePage">去众筹</div>
-          </div>
-        </div>
-        <div class="itemWrap">
-          <div class="home-item">
-            <div class="item-image"></div>
-            <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0</div>
-              <div class="progress-parent">
-                <div class="icon">50%</div>
-                <div class="progress-child"></div>
-              </div>
-              <div class="need_price">还差10000.00元</div>
-            </div>
-          </div>
-          <div class="btns">
-            <div class="myself_support" @click="jumpSelfSupport">自己支持</div>
-            <div class="go_crowd" @click="jumpSharePage">去众筹</div>
+            <div class="myself_support" @click="jumpSelfSupport" v-if="item.State == 0">自己支持</div>
+            <div class="go_crowd" @click="jumpSharePage" v-if="item.State == 0">去众筹</div>
+            <div class="write_message" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+            <div class="deposit" v-if="item.State == 2" @click="payLast()">补齐尾款</div>
           </div>
         </div>
       </div>
       <div v-if="selectedTab.key == 'successful'">
-        <div class="itemWrap">
+        <div class="itemWrap" v-for="(item,index) in successfulItems">
           <div class="home-item">
-            <div class="item-image"></div>
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.SmallImgUrl+')' ,backgroudSize:'cover'}"></div>
             <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0</div>
+              <div class="tittle">{{item.MName}}</div>
+              <div class="price">￥{{item.Price}}</div>
               <div class="progress-parent">
-                <div class="icon">100%</div>
-                <div style="width: 100%;" class="progress-child"></div>
+                <div class="icon">{{item.Rate}}</div>
+                <div class="progress-child" :style="{width:item.Rate}"></div>
               </div>
-              <div class="need_price">还差10000.00元</div>
+              <div class="need_price">还差{{item.Unfinished}}元</div>
             </div>
           </div>
           <div class="btns">
-            <div class="write_message">填写报名信息</div>
+            <div class="myself_support" @click="jumpSelfSupport" v-if="item.State == 0">自己支持</div>
+            <div class="go_crowd" @click="jumpSharePage" v-if="item.State == 0">去众筹</div>
+            <div class="write_message" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+            <div class="deposit" v-if="item.State == 2" @click="payLast()">补齐尾款</div>
           </div>
         </div>
       </div>
       <div v-if="selectedTab.key == 'failure'">
-        <div class="itemWrap">
+        <div class="itemWrap" v-for="(item,index) in failureItems">
           <div class="home-item">
-            <div class="item-image"></div>
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.SmallImgUrl+')' ,backgroudSize:'cover'}"></div>
             <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0</div>
+              <div class="tittle">{{item.MName}}</div>
+              <div class="price">￥{{item.Price}}</div>
               <div class="progress-parent">
-                <div class="icon">50%</div>
-                <div class="progress-child"></div>
+                <div class="icon">{{item.Rate}}</div>
+                <div class="progress-child" :style="{width:item.Rate}"></div>
               </div>
-              <div class="need_price">还差10000.00元</div>
+              <div class="need_price">还差{{item.Unfinished}}元</div>
             </div>
           </div>
           <div class="btns">
-            <div class="deposit">补齐尾款</div>
+            <div class="myself_support" @click="jumpSelfSupport" v-if="item.State == 0">自己支持</div>
+            <div class="go_crowd" @click="jumpSharePage" v-if="item.State == 0">去众筹</div>
+            <div class="write_message" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+            <div class="deposit" v-if="item.State == 2" @click="payLast()">补齐尾款</div>
           </div>
         </div>
       </div>
@@ -83,39 +76,208 @@
   </div>
 </template>
 <script>
-  import { mapGetters } from 'vuex';
+  import {mapGetters} from 'vuex';
   import houseBtn from './common/house-btn.vue';
   import houseHead from './common/house-head.vue';
   import houseSort from './common/house-sort.vue';
-  export default{
+  import {util} from '../assets/js/util';
+
+  export default {
     template: '.myCrowd',
     data: function () {
       return {
-        selectedTab:{value: '进行中', key: 'crowding'},
-        tabList:[
+        selectedTab: {value: '进行中', key: 'crowding'},
+        tabList: [
           {value: '进行中', key: 'crowding'},
           {value: '成功', key: 'successful'},
           {value: '失败', key: 'failure'}
-        ]
+        ],
+        crowdingItems: [],
+        successfulItems: [],
+        failureItems: []
       };
     },
     computed: {
+      ...mapGetters([
+        'getBaseUrl',
+        'getSelectRoute'
+      ])
     },
-    components: { houseHead, houseBtn, houseSort},
+    components: {houseHead, houseBtn, houseSort},
     methods: {
-      init () {
+      init() {
         window.changeTitle('我的众筹');
+        this.getCrowding();
       },
-      tabClick (tab) {
+      tabClick(tab) {
         this.selectedTab = tab;
+        let {key = ''} = tab;
+        if (key == 'crowding') {
+          this.getCrowding()
+        }
+        else if (key == 'successful') {
+          this.getSuccessfulItems()
+        }
+        else if (key == 'failure') {
+          this.getFailureItems();
+        }
       },
-      jumpSelfSupport (){
+      jumpSelfSupport() {
         this.$router.push('/selfSupport');
       },
-      jumpSharePage (){
+      jumpSharePage() {
         this.$router.push('/sharePage');
-      }
+      },
+      goInfo() {
+      },
+      payLast() {
+      },
+      getCrowding() { // 获取正在众筹
+        let jsoncontent = {
+          condition: [
+            {
+              key: 'openid',
+              values: "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+              oprate: '='
+            },
+            {"key": "State", "values": "0", "oprate": "="}
+          ]
+        };
+        let data = {
+          data: {
+            Action: 'getlistwxpersondetail',
+            jsoncontent: JSON.stringify(jsoncontent)
+          },
+          url: this.getBaseUrl + 'CommonHandler/CrowdFundOrderHandler.ashx'
+        };
+        util.fetchData(data).then(res => {
+          if (res.data.result == 0) {
+            console.log('data', res.data);
+            this.crowdingItems = [
+              {
+                "Id": 1,
+                "CId": "37D072C9F0BB4044AE061DD52C9673E9",
+                "MId": "F689820F66C94B33AA04ED0D3E9BCA74",
+                "UserName": "姓名",
+                "openid": "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+                "BeginTime": "2018-06-25 00:04:08",
+                "Unfinished": "473.0",
+                "SmallImgUrl": "ueditor/net/upload/image/20180715/6366727671960796523812055.gif",
+                "FirstImgUrl": "ueditor/net/upload/image/20180715/6366727671768523272898970.gif",
+                "headimgurl": "http://thirdwx.qlogo.cn/mmopen/vi_32/8fC42gYUBu8eSQq4VXecGjRFoO4rrCeWM9kiaEeYZt6iavQvJFE465kyFheqBiaKhUibUYjJbE1DgC77dpH7mCNtJw/132",
+                "Declaration": "",
+                "State": 0,
+                "MName": "王瑞锡赛事",
+                "Rate": "5.4%",
+                "Price": "500.00",
+                "Sumprice": "27.00",
+                "NickName": "王国壮"
+              }
+            ];
+          }
+          else {
+
+          }
+        });
+      },
+      getSuccessfulItems() {
+        let jsoncontent = {
+          condition: [
+            {
+              key: 'openid',
+              values: "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+              oprate: '='
+            },
+            {"key": "State", "values": "1", "oprate": "="}
+          ]
+        };
+        let data = {
+          data: {
+            Action: 'getlistwxpersondetail',
+            jsoncontent: JSON.stringify(jsoncontent)
+          },
+          url: this.getBaseUrl + 'CommonHandler/CrowdFundOrderHandler.ashx'
+        };
+        util.fetchData(data).then(res => {
+          if (res.data.result == 0) {
+            console.log('data', res.data);
+            this.successfulItems = [
+              {
+                "Id": 1,
+                "CId": "37D072C9F0BB4044AE061DD52C9673E9",
+                "MId": "F689820F66C94B33AA04ED0D3E9BCA74",
+                "UserName": "姓名",
+                "openid": "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+                "BeginTime": "2018-06-25 00:04:08",
+                "Unfinished": "0",
+                "SmallImgUrl": "ueditor/net/upload/image/20180715/6366727671960796523812055.gif",
+                "FirstImgUrl": "ueditor/net/upload/image/20180715/6366727671768523272898970.gif",
+                "headimgurl": "http://thirdwx.qlogo.cn/mmopen/vi_32/8fC42gYUBu8eSQq4VXecGjRFoO4rrCeWM9kiaEeYZt6iavQvJFE465kyFheqBiaKhUibUYjJbE1DgC77dpH7mCNtJw/132",
+                "Declaration": "",
+                "State": 1,
+                "MName": "王瑞锡赛事",
+                "Rate": "100%",
+                "Price": "500.00",
+                "Sumprice": "500.00",
+                "NickName": "王国壮"
+              }
+            ];
+          }
+          else {
+
+          }
+        });
+      },
+      getFailureItems() {
+        let jsoncontent = {
+          condition: [
+            {
+              key: 'openid',
+              values: "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+              oprate: '='
+            },
+            {"key": "State", "values": "2", "oprate": "="}
+          ]
+        };
+        let data = {
+          data: {
+            Action: 'getlistwxpersondetail',
+            jsoncontent: JSON.stringify(jsoncontent)
+          },
+          url: this.getBaseUrl + 'CommonHandler/CrowdFundOrderHandler.ashx'
+        };
+        util.fetchData(data).then(res => {
+          if (res.data.result == 0) {
+            console.log('data', res.data);
+            this.failureItems = [
+              {
+                "Id": 1,
+                "CId": "37D072C9F0BB4044AE061DD52C9673E9",
+                "MId": "F689820F66C94B33AA04ED0D3E9BCA74",
+                "UserName": "姓名",
+                "openid": "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+                "BeginTime": "2018-06-25 00:04:08",
+                "Unfinished": "100.00",
+                "SmallImgUrl": "ueditor/net/upload/image/20180715/6366727671960796523812055.gif",
+                "FirstImgUrl": "ueditor/net/upload/image/20180715/6366727671768523272898970.gif",
+                "headimgurl": "http://thirdwx.qlogo.cn/mmopen/vi_32/8fC42gYUBu8eSQq4VXecGjRFoO4rrCeWM9kiaEeYZt6iavQvJFE465kyFheqBiaKhUibUYjJbE1DgC77dpH7mCNtJw/132",
+                "Declaration": "",
+                "State": 2,
+                "MName": "王瑞锡赛事",
+                "Rate": "80%",
+                "Price": "500.00",
+                "Sumprice": "400.00",
+                "NickName": "王国壮"
+              }
+            ];
+          }
+          else {
+
+          }
+        })
+      },
     },
+
     mounted: function () {
       this.init()
     }
--- src/components/myOrder.vue
+++ src/components/myOrder.vue
@@ -4,61 +4,10 @@
     <houseSort :tabList="tabList" @tabClick="tabClick ($event)"></houseSort>
     <div class="content">
       <div v-if="selectedTab.key == 'unComplete'">
-        <div class="itemWrap">
+        <div class="itemWrap" v-for="(item,i) in unCompleteOrders">
           <div class="home-item">
-            <div class="item-image"></div>
-            <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0<span>X2件</span></div>
-            </div>
-          </div>
-          <div class="total">
-            <!--<div class="tittle">等待付款</div>-->
-            <div class="text">合计：<span style="color: red;">￥25600:00</span></div>
-            <div class="set">
-              <div class="payBtn">去支付</div>
-            </div>
-          </div>
-        </div>
-        <div class="itemWrap">
-          <div class="home-item">
-            <div class="item-image"></div>
-            <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0<span>X2件</span></div>
-            </div>
-          </div>
-          <div class="total">
-            <div class="text">合计：<span style="color: red;">￥25600:00</span></div>
-            <!--<div class="tittle">等待付款</div>-->
-            <div class="set">
-              <div class="payBtn">去支付</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div v-if="selectedTab.key == 'complete'">
-        <div class="itemWrap">
-          <div class="home-item">
-            <div class="item-image"></div>
-            <div class="item-des">
-              <div class="tittle">2018第一届驯鹿丝绸路108城市戈壁挑战赛</div>
-              <div class="price">￥12999.0<span>X2件</span></div>
-            </div>
-          </div>
-          <div class="total">
-            <!--<div class="tittle">等待付款</div>-->
-            <div class="text">合计：<span style="color: red;">￥25600:00</span></div>
-            <div class="set">
-              <div class="payBtn">填写报名信息</div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div v-if="selectedTab.key == 'allOrders'">
-        <div class="itemWrap" v-for="(item,i) in allOrders">
-          <div class="home-item">
-            <div class="item-image"></div>
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.FirstImgUrl+')' ,backgroudSize:'cover'}"></div>
             <div class="item-des">
               <div class="tittle">{{item.MName}}</div>
               <div class="state">{{ item.State | orderState}}</div>
@@ -69,8 +18,50 @@
             <!--<div class="tittle">等待付款</div>-->
             <div class="text">合计：<span style="color: red;">{{item.TotalPrice?`￥${item.TotalPrice}`:''}}</span></div>
             <div class="set">
-              <div class="payBtn" v-if="item.State == 1">填写报名信息</div>
-              <div class="payBtn" v-if="item.State == 0">去支付</div>
+              <div class="payBtn" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+              <div class="payBtn" v-if="item.State == 0" @click="goPay()">去支付</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="selectedTab.key == 'complete'">
+        <div class="itemWrap" v-for="(item,i) in completeOrders">
+          <div class="home-item">
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.SmallImgUrl+')' ,backgroudSize:'cover'}"></div>
+            <div class="item-des">
+              <div class="tittle">{{item.MName}}</div>
+              <div class="state">{{ item.State | orderState}}</div>
+              <div class="price">￥{{item.Price}}<span>{{item.Num?`X${item.Num}`:''}}件</span></div>
+            </div>
+          </div>
+          <div class="total">
+            <!--<div class="tittle">等待付款</div>-->
+            <div class="text">合计：<span style="color: red;">{{item.TotalPrice?`￥${item.TotalPrice}`:''}}</span></div>
+            <div class="set">
+              <div class="payBtn" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+              <div class="payBtn" v-if="item.State == 0" @click="goPay()">去支付</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="selectedTab.key == 'allOrders'">
+        <div class="itemWrap" v-for="(item,i) in allOrders">
+          <div class="home-item">
+            <div class="item-image"
+                 :style="{background:'url('+getBaseUrl+item.SmallImgUrl+')' ,backgroudSize:'cover'}"></div>
+            <div class="item-des">
+              <div class="tittle">{{item.MName}}</div>
+              <div class="state">{{ item.State | orderState}}</div>
+              <div class="price">￥{{item.Price}}<span>{{item.Num?`X${item.Num}`:''}}件</span></div>
+            </div>
+          </div>
+          <div class="total">
+            <!--<div class="tittle">等待付款</div>-->
+            <div class="text">合计：<span style="color: red;">{{item.TotalPrice?`￥${item.TotalPrice}`:''}}</span></div>
+            <div class="set">
+              <div class="payBtn" v-if="item.State == 1" @click="goInfo()">填写报名信息</div>
+              <div class="payBtn" v-if="item.State == 0" @click="goPay()">去支付</div>
             </div>
           </div>
         </div>
@@ -88,7 +79,7 @@
     template: '.myOrder',
     data: function () {
       return {
-        selectedTab:{value: '全部订单', key: 'allOrders'},
+        selectedTab:{value: '待支付', key: 'unComplete'},
         tabList:[
           {value: '待支付', key: 'unComplete'},
           {value: '已完成', key: 'complete'},
@@ -109,10 +100,21 @@
     methods: {
       init () {
         window.changeTitle('我的订单');
-        this.getALLOrders();
+        //this.getALLOrders();
+        this.getUnComplete();
       },
       tabClick (tab) {
         this.selectedTab = tab;
+        let {key = ''} = tab;
+        if (key == 'unComplete') {
+          this.getUnComplete();
+        }
+        else if (key == 'complete') {
+          this.getComplete();
+        }
+        else if (key == 'allOrders') {
+          this.getALLOrders();
+        }
       },
       getALLOrders () {// 获取全部订单
         let jsoncontent ={
@@ -181,7 +183,105 @@
 
           }
         });
-      }
+      },
+      getUnComplete () { // 获取待支付订单
+        let jsoncontent ={
+          condition:[
+            {
+              key:'openid',
+              values:"ol7xB1grgNlrobJfBQEKMRFEMrVY",
+              oprate:'='
+            },
+            {"key":"State","values":"0","oprate":"="}
+          ]
+        } ;
+        let data = {
+          data:{
+            Action:'getlistwxmyorder',
+            jsoncontent:JSON.stringify(jsoncontent)
+          },
+          url:this.getBaseUrl + 'CommonHandler/OrderHandler.ashx'
+        };
+        util.fetchData (data).then(res => {
+          if (res.data.result == 0) {
+            console.log('data',res.data);
+            this.unCompleteOrders =  [
+              {
+                "Id": 4,
+                "OId": "A2EE5E42B9004E3D9FDC830E500E3C2C",
+                "MId": "D3438C9EC51341258E53460167DD7466",
+                "TId": "A8AEA2803D2E4E42951B7689A3C71A91",
+                "PayTime": "2018-06-23 01:09:02",
+                "Price": 120.00,
+                "Num": 3,
+                "TotalPrice": 360.00,
+                "State": 0,
+                "Remark": "",
+                "openid": "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+                "username": "姓名",
+                "phone": "联系电话",
+                "nickname": "王国壮",
+                "headimgurl": "http://thirdwx.qlogo.cn/mmopen/vi_32/8fC42gYUBu8eSQq4VXecGjRFoO4rrCeWM9kiaEeYZt6iavQvJFE465kyFheqBiaKhUibUYjJbE1DgC77dpH7mCNtJw/132",
+                "SmallImgUrl": "",
+                "FirstImgUrl": "ueditor/net/upload/image/20180708/6366660768052374223029545.gif",
+                "MName": "赛事111"
+              }
+            ];
+          }
+          else {
+
+          }
+        });
+      },
+      getComplete () { // 获取完成订单
+        let jsoncontent ={
+          condition:[
+            {
+              key:'openid',
+              values:"ol7xB1grgNlrobJfBQEKMRFEMrVY",
+              oprate:'='
+            },
+            {"key":"State","values":"0","oprate":"="}
+          ]
+        } ;
+        let data = {
+          data:{
+            Action:'getlistwxmyorder',
+            jsoncontent:JSON.stringify(jsoncontent)
+          },
+          url:this.getBaseUrl + 'CommonHandler/OrderHandler.ashx'
+        };
+        util.fetchData (data).then(res => {
+          if (res.data.result == 0) {
+            console.log('data',res.data);
+            this.completeOrders =  [
+              {
+                "Id": 4,
+                "OId": "A2EE5E42B9004E3D9FDC830E500E3C2C",
+                "MId": "D3438C9EC51341258E53460167DD7466",
+                "TId": "A8AEA2803D2E4E42951B7689A3C71A91",
+                "PayTime": "2018-06-23 01:09:02",
+                "Price": 120.00,
+                "Num": 3,
+                "TotalPrice": 360.00,
+                "State": 1,
+                "Remark": "",
+                "openid": "ol7xB1grgNlrobJfBQEKMRFEMrVY",
+                "username": "姓名",
+                "phone": "联系电话",
+                "nickname": "王国壮",
+                "headimgurl": "http://thirdwx.qlogo.cn/mmopen/vi_32/8fC42gYUBu8eSQq4VXecGjRFoO4rrCeWM9kiaEeYZt6iavQvJFE465kyFheqBiaKhUibUYjJbE1DgC77dpH7mCNtJw/132",
+                "SmallImgUrl": "",
+                "FirstImgUrl": "ueditor/net/upload/image/20180708/6366660768052374223029545.gif",
+                "MName": "赛事111"
+              }
+            ];
+          }
+          else {
+
+          }
+        });
+      },
     },
     mounted: function () {
       this.init()
